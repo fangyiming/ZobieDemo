73bb3d5972510d91fdfe45f84804cd93bcbc8e28
fangyiming <1043630181@qq.com>
2019-02-12 15:15:44 +0800

    first commit

---
+++ src/com/common/PlantType.java
@@ -0,0 +1,31 @@
+package com.common;
+
+import java.awt.Image;
+
+import com.util.ImageUtil;
+
+public enum PlantType {
+	NONE(null),
+	SunFlower(ImageUtil.loadImage("sun_flower1.png")),    //调用了PlantType的构造方法，此时moveImg是sun_flower1.png
+	SingleBullet(ImageUtil.loadImage("single_bullet_plant.png")),//调用了PlantType的构造方法，此时moveImg是single_bullet_plant.png
+	Cherry(null),
+	SmallStone(null),
+	Mine(null),
+	ColdBullet(null),
+	Eat(null),
+	DoubBullet(null);
+	
+	private Image plantImg;
+	
+	PlantType(Image plantImg) {
+		this.plantImg = plantImg;
+	}
+
+	public Image getPlantImg() {
+		return plantImg;
+	}
+	
+	
+	
+	
+}
